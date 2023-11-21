fe408c48df12ffa2754ac1f2d9f5fa5a681914d3
Christophe M <94084316+KrisCorporation@users.noreply.github.com>
2023-11-21 21:43:16 +0100

    Add files via upload

---
+++ errorpage.tsx
@@ -0,0 +1,12 @@
+import './errors.css'
+
+function Errorpage(){
+
+    return(
+        <div className='errorpage'>
+            <div className='errortext'><span className='oups'>Oups,</span> Error page not found...</div>
+        </div>
+    )
+}
+
+export default Errorpage
